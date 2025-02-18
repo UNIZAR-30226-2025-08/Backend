f7b3f2098f58df932ff636767080d8b487afe4f0
Juan González Pardo <162998863+870313@users.noreply.github.com>
2025-02-18 16:26:39 +0100

    Partida.js Traducida al español
    Revisar si os parece bien la traducción, si no veis ningun fallo podeis mergear las ramas y quitar el pull reques

--- Partida.js
+++ Partida.js
@@ -1,272 +1,266 @@
-class Game {
-  constructor(gameId, players) {
-    this.gameId = gameId;
-    this.status = 'ongoing'; // Estado de la partida ('ongoing', 'completed')
-    this.turn = 'night'; // Fase actual: 'day' o 'night'. La partida empieza en la noche
-    this.players = players.map(player => ({ // Array de jugadores con sus roles
-      id: player.id,
-      role: player.role,
-      alive: true,
-      isSheriff: false, // ¿Es alguacil?
-      usedPotionHeal: player.role === 'bruja' ? false : undefined, // Si la bruja usó su poción de vida
-      usedPotionKill: player.role === 'bruja' ? false : undefined, // Si la bruja usó su poción de muerte
-      hasSeen: player.role === 'vidente' ? false : undefined, // La vidente puede ver el rol de un jugador por la noche
+class Juego {
+  constructor(idPartida, jugadores) {
+    this.idPartida = idPartida;
+    this.estado = 'en curso'; // Estado de la partida ('en curso', 'completada')
+    this.turno = 'noche'; // Fase actual: 'día' o 'noche'. La partida empieza en la noche
+    this.jugadores = jugadores.map(jugador => ({
+      id: jugador.id,
+      rol: jugador.rol, // Rol asignado (por ejemplo, 'bruja', 'vidente', 'lobo', etc.)
+      vivo: true,
+      esAlguacil: false, // ¿Es alguacil?
+      usoPociónDeVida: jugador.rol === 'bruja' ? false : undefined, // Si la bruja usó su poción de vida
+      usoPociónDeMuerte: jugador.rol === 'bruja' ? false : undefined, // Si la bruja usó su poción de muerte
+      haVisto: jugador.rol === 'vidente' ? false : undefined, // La vidente puede ver el rol de un jugador por la noche
     }));
     this.chat = []; // Mensajes de chat (día: global, noche: solo lobos)
-    this.sheriffVotes = {}; // Votos para elegir al alguacil
-    this.votes = {}; // Votos de los jugadores durante el día
-    this.sheriffRepeatVote = false; // Para empates en elección de alguacil
-    this.repeatVote = false; // Controla si hubo un empate previo en las votaciones del día
-    this.nightVotes = {}; // Votos de los lobos durante la noche
-    this.eliminationQueue = []; // Cola de eliminación al final del turno
+    this.votosAlAlguacil = {}; // Votos para elegir al alguacil
+    this.votos = {}; // Votos de los jugadores durante el día
+    this.votoRepetidoAlAlguacil = false; // Para empates en elección de alguacil
+    this.votoRepetido = false; // Controla si hubo un empate previo en las votaciones del día
+    this.votosNocturnos = {}; // Votos de los lobos durante la noche
+    this.colaEliminacion = []; // Cola de eliminación al final del turno
   }
 
-  // Precondición: antes de llamar a este método, se deben de aplicar las eliminaciones del turno actual
+  // Precondición: antes de llamar a este método, se deben aplicar las eliminaciones del turno actual
   // y comprobar si la partida ha terminado o no.
-  // Para ello se debería de llamar antes al método applyEliminations() de esta misma clase Game.
-  // Si applyEliminations() detecta que la partida terminó, no se llama a nextTurn().
-  // Solo si la partida sigue en curso, entonces se ejecuta nextTurn().
+  // Para ello se debería llamar antes al método aplicarEliminaciones() de esta misma clase Juego.
+  // Si aplicarEliminaciones() detecta que la partida terminó, no se llama a siguienteTurno().
+  // Solo si la partida sigue en curso, se ejecuta siguienteTurno().
   // Método para cambiar turnos
-  nextTurn() {
-    // !!!! this.applyEliminations(); // Ejecutar eliminaciones pendientes !!!! PONER PARA LOS TEST
-    this.turn = this.turn === 'night' ? 'day' : 'night';
-    this.votes = {}; // Reiniciar votos en el día
-    this.nightVotes = {}; // Reiniciar votos de los lobos en la noche
-    this.sheriffVotes = {}; // Reiniciar votos para elegir alguacil !!!!!!!!!!!! NO SÉ SI ES NECESARIO !!!!!!!!!!!!
+  siguienteTurno() {
+    // !!!! this.aplicarEliminaciones(); // Ejecutar eliminaciones pendientes (PONER PARA LOS TEST)
+    this.turno = this.turno === 'noche' ? 'día' : 'noche';
+    this.votos = {}; // Reiniciar votos en el día
+    this.votosNocturnos = {}; // Reiniciar votos de los lobos en la noche
+    this.votosAlAlguacil = {}; // Reiniciar votos para elegir alguacil
 
     // Reiniciar la habilidad de la vidente cuando comienza una nueva noche
-    if (this.turn === 'night') {
-      this.players.forEach(player => {
-        if (player.role === 'vidente') {
-          player.hasSeen = false;
+    if (this.turno === 'noche') {
+      this.jugadores.forEach(jugador => {
+        if (jugador.rol === 'vidente') {
+          jugador.haVisto = false;
         }
       });
     }
   }
 
   // Método para registrar un mensaje en el chat
-  addChatMessage(playerId, message) {
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive) return;
+  agregarMensajeChat(idJugador, mensaje) {
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo) return;
 
-    if (this.turn === 'day' || player.role === 'lobo') {
-      this.chat.push({ playerId, message, timestamp: Date.now() });
+    if (this.turno === 'día' || jugador.rol === 'lobo') {
+      this.chat.push({ idJugador, mensaje, timestamp: Date.now() });
     }
   }
 
   // Método para elegir al alguacil
-  electSheriff() {
-    const voteCounts = {};
-    for (const voter in this.sheriffVotes) {
-      const voted = this.sheriffVotes[voter];
-      voteCounts[voted] = (voteCounts[voted] || 0) + 1;
+  elegirAlguacil() {
+    const conteoVotos = {};
+    for (const votante in this.votosAlAlguacil) {
+      const votado = this.votosAlAlguacil[votante];
+      conteoVotos[votado] = (conteoVotos[votado] || 0) + 1;
     }
 
-    let maxVotes = 0;
-    let candidates = [];
+    let maxVotos = 0;
+    let candidatos = [];
 
-    for (const playerId in voteCounts) {
-      if (voteCounts[playerId] > maxVotes) {
-        maxVotes = voteCounts[playerId];
-        candidates = [playerId];
-      } else if (voteCounts[playerId] === maxVotes) {
-        candidates.push(playerId);
+    for (const idJugador in conteoVotos) {
+      if (conteoVotos[idJugador] > maxVotos) {
+        maxVotos = conteoVotos[idJugador];
+        candidatos = [idJugador];
+      } else if (conteoVotos[idJugador] === maxVotos) {
+        candidatos.push(idJugador);
       }
     }
 
-    if (candidates.length === 1) {
-      this.players.forEach(p => (p.isSheriff = false)); // Asegurarse de que ningún jugador sea alguacil
-      const sheriff = this.players.find(p => p.id === candidates[0]);
-      if (sheriff) sheriff.isSheriff = true;
-      this.sheriffRepeatVote = false; // Reiniciar flag de repetición
-      return `El jugador ${candidates[0]} ha sido elegido como alguacil.`;
+    if (candidatos.length === 1) {
+      this.jugadores.forEach(j => (j.esAlguacil = false)); // Asegurarse de que ningún jugador sea alguacil
+      const alguacil = this.jugadores.find(j => j.id === candidatos[0]);
+      if (alguacil) alguacil.esAlguacil = true;
+      this.votoRepetidoAlAlguacil = false; // Reiniciar flag de repetición
+      return `El jugador ${candidatos[0]} ha sido elegido como alguacil.`;
     } else {
-      if (this.sheriffRepeatVote) {
+      if (this.votoRepetidoAlAlguacil) {
         return 'Segundo empate consecutivo, no se elige alguacil.';
       } else {
-        this.sheriffRepeatVote = true;
-        this.sheriffVotes = {}; // Resetear votos para repetir elección
+        this.votoRepetidoAlAlguacil = true;
+        this.votosAlAlguacil = {}; // Resetear votos para repetir elección
         return 'Empate en la elección del alguacil, se repiten las votaciones.';
       }
     }
   }
 
   // Método para votar al alguacil
-  voteSheriff(playerId, targetId) {
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive) return;
+  votarAlAlguacil(idJugador, idObjetivo) {
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo) return;
 
-    this.sheriffVotes[playerId] = targetId;
+    this.votosAlAlguacil[idJugador] = idObjetivo;
   }
 
   // Método para manejar las votaciones de los jugadores durante el día
   // El voto del alguacil cuenta como dos
-  vote(playerId, targetId) {
-    if (this.turn !== 'day') return;
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive) return;
+  votar(idJugador, idObjetivo) {
+    if (this.turno !== 'día') return;
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo) return;
 
-    this.votes[playerId] = targetId;
-    if (player.isSheriff) {
-      this.votes[`sheriff_${playerId}`] = targetId; // Doble voto (voto extra del alguacil)
+    this.votos[idJugador] = idObjetivo;
+    if (jugador.esAlguacil) {
+      this.votos[`alguacil_${idJugador}`] = idObjetivo; // Doble voto (voto extra del alguacil)
     }
   }
 
   // Votación de los lobos en la noche
-  voteNight(playerId, targetId) {
-    if (this.turn !== 'night') return;
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive || player.role !== 'lobo') return;
+  votarNoche(idJugador, idObjetivo) {
+    if (this.turno !== 'noche') return;
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo || jugador.rol !== 'lobo') return;
 
-    this.nightVotes[playerId] = targetId;
+    this.votosNocturnos[idJugador] = idObjetivo;
   }
 
   // La vidente obtiene el rol de un jugador
-  seerReveal(playerId, targetId) {
-    if (this.turn !== 'night') return 'No es de noche.';
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive || player.role !== 'vidente' || player.hasSeen) return 'No puedes usar esta habilidad.';
+  revelarVidente(idJugador, idObjetivo) {
+    if (this.turno !== 'noche') return 'No es de noche.';
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo || jugador.rol !== 'vidente' || jugador.haVisto) return 'No puedes usar esta habilidad.';
 
-    const target = this.players.find(p => p.id === targetId);
-    if (!target || !target.alive) 
+    const objetivo = this.jugadores.find(j => j.id === idObjetivo);
+    if (!objetivo || !objetivo.vivo)
       return 'Solo puedes ver el rol de un jugador vivo.';
 
-    player.hasSeen = true; // La vidente solo puede ver un jugador por noche
-    
-    return `El jugador ${targetId} es ${target.role}.`;
+    jugador.haVisto = true; // La vidente solo puede ver un jugador por noche
+
+    return `El jugador ${idObjetivo} es ${objetivo.rol}.`;
   }
 
   // Método para que la bruja use sus pociones
-  useWitchPotion(playerId, type, targetId) {
-    const player = this.players.find(p => p.id === playerId);
-    if (!player || !player.alive || player.role !== 'bruja') return;
-    
-    if (type === 'heal' && !player.usedPotionHeal) {
-      player.usedPotionHeal = true;
-      this.eliminationQueue = this.eliminationQueue.filter(id => id !== targetId); // Cancela la muerte de los lobos
-    } else if (type === 'kill' && !player.usedPotionKill) {
-      player.usedPotionKill = true;
-      this.queueElimination(targetId); // Se elimina al final del turno
+  usarPocionBruja(idJugador, tipo, idObjetivo) {
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (!jugador || !jugador.vivo || jugador.rol !== 'bruja') return;
+
+    if (tipo === 'heal' && !jugador.usoPociónDeVida) {
+      jugador.usoPociónDeVida = true;
+      this.colaEliminacion = this.colaEliminacion.filter(id => id !== idObjetivo); // Cancela la muerte de los lobos
+    } else if (tipo === 'kill' && !jugador.usoPociónDeMuerte) {
+      jugador.usoPociónDeMuerte = true;
+      this.agregarAColaEliminacion(idObjetivo); // Se elimina al final del turno
     }
   }
 
   // Resolver votos del día
-  resolveDayVotes() {
-    const voteCounts = {};
-    
-    for (const voter in this.votes) {
-      const voted = this.votes[voter];
-      voteCounts[voted] = (voteCounts[voted] || 0) + 1;
+  resolverVotosDia() {
+    const conteoVotos = {};
+
+    for (const votante in this.votos) {
+      const votado = this.votos[votante];
+      conteoVotos[votado] = (conteoVotos[votado] || 0) + 1;
     }
-  
-    // Determinar el jugador con más votos
-    let maxVotes = 0;
-    let candidates = [];
-    
-    for (const playerId in voteCounts) {
-      if (voteCounts[playerId] > maxVotes) {
-        maxVotes = voteCounts[playerId];
-        candidates = [playerId];
-      } else if (voteCounts[playerId] === maxVotes) {
-        candidates.push(playerId);
+
+    let maxVotos = 0;
+    let candidatos = [];
+
+    for (const idJugador in conteoVotos) {
+      if (conteoVotos[idJugador] > maxVotos) {
+        maxVotos = conteoVotos[idJugador];
+        candidatos = [idJugador];
+      } else if (conteoVotos[idJugador] === maxVotos) {
+        candidatos.push(idJugador);
       }
     }
-  
-    if (candidates.length === 1) {
-      this.queueElimination(candidates[0]);
-      this.repeatVote = false; // Se reinicia al resolver una votación sin empate
-      return `El jugador ${candidates[0]} será eliminado al final del día.`;
+
+    if (candidatos.length === 1) {
+      this.agregarAColaEliminacion(candidatos[0]);
+      this.votoRepetido = false; // Se reinicia al resolver una votación sin empate
+      return `El jugador ${candidatos[0]} será eliminado al final del día.`;
     } else {
-      if (this.repeatVote) {
+      if (this.votoRepetido) {
         return 'Segundo empate consecutivo, nadie es eliminado.';
       } else {
-        this.repeatVote = true;
-        this.votes = {}; // Reiniciar votos para repetir la votación
+        this.votoRepetido = true;
+        this.votos = {}; // Reiniciar votos para repetir la votación
         return 'Empate, se repiten las votaciones.';
       }
     }
   }
 
   // Resolver la decisión de los lobos (se elimina al final del turno)
-  resolveNightVotes() {
-    const voteCounts = {};
-    
-    for (const wolfId in this.nightVotes) {
-      const voted = this.nightVotes[wolfId];
-      if (voted) voteCounts[voted] = (voteCounts[voted] || 0) + 1;
+  resolverVotosNoche() {
+    const conteoVotos = {};
+
+    for (const idLobo in this.votosNocturnos) {
+      const votado = this.votosNocturnos[idLobo];
+      if (votado) conteoVotos[votado] = (conteoVotos[votado] || 0) + 1;
     }
-    
+
     // Buscar la víctima con unanimidad
-    let selectedVictim = null;
-    let totalWolves = this.players.filter(p => p.role === 'lobo' && p.alive).length; // Número de lobos vivos
-    for (const [playerId, count] of Object.entries(voteCounts)) {
-      if (count === totalWolves) {
-        selectedVictim = playerId;
+    let victimaSeleccionada = null;
+    const totalLobos = this.jugadores.filter(j => j.rol === 'lobo' && j.vivo).length; // Número de lobos vivos
+    for (const [idJugador, cuenta] of Object.entries(conteoVotos)) {
+      if (cuenta === totalLobos) {
+        victimaSeleccionada = idJugador;
       }
     }
-  
-    if (selectedVictim) {
-      this.queueElimination(selectedVictim);
-      return `Los lobos atacaron al jugador ${selectedVictim}. Será eliminado al final de la noche.`;
+
+    if (victimaSeleccionada) {
+      this.agregarAColaEliminacion(victimaSeleccionada);
+      return `Los lobos atacaron al jugador ${victimaSeleccionada}. Será eliminado al final de la noche.`;
     } else {
       return 'Los lobos no se pusieron de acuerdo, no hay víctima esta noche.';
     }
   }
 
   // Cola de eliminación para el final del turno
-  queueElimination(playerId) {
-    const player = this.players.find(p => p.id === playerId);
-    if (player && player.alive) {
-      this.eliminationQueue.push(playerId);
+  agregarAColaEliminacion(idJugador) {
+    const jugador = this.jugadores.find(j => j.id === idJugador);
+    if (jugador && jugador.vivo) {
+      this.colaEliminacion.push(idJugador);
     }
   }
 
   // Aplicar eliminaciones al final del turno
-  applyEliminations() {
-    console.log("Aplicando eliminaciones a:", this.eliminationQueue);
-    this.eliminationQueue.forEach(playerId => {
-      const player = this.players.find(p => p.id === playerId);
-      if (player) {
-        console.log(`Eliminando al jugador ${playerId}`);
-        player.alive = false;
+  aplicarEliminaciones() {
+    console.log("Aplicando eliminaciones a:", this.colaEliminacion);
+    this.colaEliminacion.forEach(idJugador => {
+      const jugador = this.jugadores.find(j => j.id === idJugador);
+      if (jugador) {
+        console.log(`Eliminando al jugador ${idJugador}`);
+        jugador.vivo = false;
       }
     });
-    this.eliminationQueue = []; // Reiniciar la cola
+    this.colaEliminacion = []; // Reiniciar la cola
 
     // Comprobar si hay un ganador y devolver el resultado
-    return this.checkVictory();
+    return this.verificarVictoria();
   }
 
-  // Verifica si la partida ha terminado. Y en caso de terminar devuelve quién ganó
-  checkVictory() {
-    const aliveWolves = this.players.filter(p => p.alive && p.role === 'lobo').length;
-    const aliveVillagers = this.players.filter(p => p.alive && p.role !== 'lobo').length;
+  // Verifica si la partida ha terminado y, en caso afirmativo, devuelve quién ganó
+  verificarVictoria() {
+    const lobosVivos = this.jugadores.filter(j => j.vivo && j.rol === 'lobo').length;
+    const aldeanosVivos = this.jugadores.filter(j => j.vivo && j.rol !== 'lobo').length;
 
     // Ganan los aldeanos cuando no quedan lobos vivos
-    if (aliveWolves === 0 && aliveVillagers !== 0) {
-      this.status = 'completed';
+    if (lobosVivos === 0 && aldeanosVivos !== 0) {
+      this.estado = 'completada';
       return 'Los aldeanos han ganado la partida.';
-      // Podemos emitir el estado de la partida devolviendo un objeto con el estado y un mensaje
-      // return { winner: 'aldeanos', message: 'Los aldeanos han ganado la partida.' };
     }
 
     // Ganan los lobos cuando no quedan aldeanos vivos
-    if (aliveVillagers === 0 && aliveWolves !== 0) { 
-      this.status = 'completed';
+    if (aldeanosVivos === 0 && lobosVivos !== 0) {
+      this.estado = 'completada';
       return 'Los lobos han ganado la partida.';
-      // return { winner: 'lobos', message: 'Los lobos han ganado la partida.' };
     }
 
     // Empate si todos los jugadores están muertos
-    if(aliveVillagers === 0 && aliveWolves === 0) {
-      this.status = 'completed';
+    if (aldeanosVivos === 0 && lobosVivos === 0) {
+      this.estado = 'completada';
       return 'Empate, no hay ganadores.';
-      // return { winner: 'empate', message: 'Empate, no hay ganadores.' };
     }
 
     return null; // La partida sigue en curso
   }
-
 }
 
-module.exports = Game; // exportar la clase
+module.exports = Juego; // Exportar la clase
