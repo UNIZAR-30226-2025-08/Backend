95704eeed1afde2c0f61436de8073a77bcd62671
Marcosis2 <a869021@unizar.es>
2025-02-19 12:38:32 +0100

    Actualización newRF version erronea aplicarElim
    Había un error de concepto en como se llamaba a disparoCazador y elegirSucesor

--- newRF_Partida.js
+++ newRF_Partida.js
@@ -1,25 +1,3 @@
-  // Aplicar eliminaciones al final del turno
-  aplicarEliminaciones() {
-    console.log("Aplicando eliminaciones a:", this.colaEliminaciones);
-    this.colaEliminaciones.forEach(jugadorId => {
-      const jugador = this.jugadores.find(p => p.id === jugadorId);
-      if (jugador) {
-        if (jugador.esAlguacil) {
-          this.elegirSucesor();
-        }
-        if (jugador.rol === 'cazador') {
-          this.disparoCazador();
-        }
-        console.log(`Eliminando al jugador ${jugadorId}`);
-        jugador.vivo = false;
-      }
-    });
-    this.colaEliminaciones = []; // Reiniciar la cola
-
-    // Comprobar si hay un ganador y devolver el resultado
-    return this.comprobarVictoria();
-  }
-
 
     // Método del cazador para elegir a su víctima
     disparoCazador(jugadorId, type, objetivoId) {
